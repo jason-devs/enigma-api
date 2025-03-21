fb404da37ed066cc501eaf2d9db50055842da017
jason-devs <jasondevs.mail@gmail.com>
2025-03-21 19:34:56 +0000

    fix: cookie security prod

--- controllers/authController.js
+++ controllers/authController.js
@@ -36,6 +36,7 @@ const sendJWT = (user, token, res) => {
   const cookieOptions = {
     expires: new Date(Date.now() + JWT_COOKIE_EXPIRY * 24 * 60 * 60 * 1000),
     httpOnly: true,
+    secure: NODE_ENV === "production",
   };
 
   if (NODE_ENV === "production") cookieOptions.secure = true;
